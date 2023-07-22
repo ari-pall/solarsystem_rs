7a1037f65ad53887608584fe0d41a6a021faf307
I <>
2023-07-22 18:24:01 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,12 @@ use {derive_more::From,
 fn new<T: Default>() -> T { T::default() }
 fn not(v: bool) -> bool { v.not() }
 
-const MOVE_SPEED: f32 = 0.1;
+const MOVE_SPEED: f32 = 0.12;
 const LOOK_SPEED: f32 = 0.1;
 const WORLD_UP: Vec3 = vec3(0.0, 1.0, 0.0);
 
 fn conf() -> Conf {
   Conf { window_title: String::from("solar system"),
-         // window_width: 1260,
-         // window_height: 768,
          fullscreen: true,
          ..Default::default() }
 }
@@ -52,7 +50,7 @@ impl Planet {
                             r: x,
                             g: y,
                             b: z },
-             pos: vec3(x * 20.0 - 4.0, y * 20.0 - 4.0, z * 20.0 - 4.0),
+             pos: vec3(x * 20.0 - 5.0, y * 20.0 - 5.0, z * 20.0 - 5.0),
              vel: vec3(rng(-0.02, 0.02), rng(-0.02, 0.02), rng(-0.02, 0.02)),
              radius: rng(0.1, 0.3) }
   }
@@ -75,33 +73,28 @@ impl Default for Planets {
       }, i in 0..NUM_PLANETS))
   }
 }
-pub fn fold<T, G>(init: T, f: impl Fn(T, G) -> T, coll: impl IntoIterator<Item = G>) -> T {
+pub fn fold<T, G>(f: impl Fn(T, G) -> T, init: T, coll: impl IntoIterator<Item = G>) -> T {
   coll.into_iter().fold(init, f)
 }
 impl Planets {
   fn movement(self) -> Self { Self(comp!(Planet{pos: p.pos+p.vel,..p}, p in self.0)) }
   fn gravity(self) -> Self {
-    Self(fold(self.0,
-              |mut p, (i, j)| {
+    Self(fold(|mut p, (i, j)| {
                 let dist = p[i].pos.distance(p[j].pos);
                 let posdiff = p[j].pos - p[i].pos;
-                let pimass = p[i].radius.powi(3);
                 let pjmass = p[j].radius.powi(3);
                 let g = 0.003;
                 p[i].vel += g * pjmass * posdiff / dist.powi(3);
-                p[j].vel -= g * pimass * posdiff / dist.powi(3);
                 p
               },
-              comp!((i,j), i in 0..NUM_PLANETS, j in 0..i)))
+              self.0,
+              comp!((i,j), i in 0..NUM_PLANETS, j in 0..NUM_PLANETS, i != j)))
   }
 }
 struct State {
   planets: Planets,
   yaw: f32,
   pitch: f32,
-  front: Vec3,
-  right: Vec3,
-  up: Vec3,
   position: Vec3,
   orientation: Quat,
   last_mouse_position: Vec2,
@@ -109,15 +102,9 @@ struct State {
 }
 impl Default for State {
   fn default() -> Self {
-    let yaw = 1.18;
-    let pitch = 1.18;
-    let front = vec3_from_spherical_coords(yaw, pitch);
     Self { planets: Planets::default(),
-           yaw,
-           pitch,
-           front,
-           right: front.cross(WORLD_UP).normalize(),
-           up: vec3(0.0, 1.0, 0.0),
+           yaw: 1.18,
+           pitch: 1.18,
            position: vec3(0.0, 1.0, 0.0),
            orientation: Quat::default(),
            last_mouse_position: mouse_position().into(),
@@ -125,14 +112,10 @@ impl Default for State {
   }
 }
 impl State {
-  // const HI: f32 = 8.0;
-  // const LO: f32 = -Self::HI;
   fn update(self, change: Vec3, mouse_position: Vec2, delta: f32) -> Self {
     let Self { last_mouse_position,
                pitch,
                yaw,
-               front,
-               right,
                planets,
                position,
                .. } = self;
@@ -141,9 +124,6 @@ impl State {
     Self { last_mouse_position: mouse_position,
            pitch: (pitch + mouse_delta.y * delta * -LOOK_SPEED).clamp(-1.5, 1.5),
            yaw: yaw + mouse_delta.x * delta * LOOK_SPEED,
-           front: vec3_from_spherical_coords(yaw, pitch),
-           right: front.cross(WORLD_UP).normalize(),
-           up: right.cross(front).normalize(),
            planets: planets.gravity().movement(),
            position: position + change,
            ..self }
@@ -170,21 +150,16 @@ async fn main() {
       set_cursor_grab(state.grabbed);
       show_mouse(!state.grabbed);
     }
-    // let change = MOVE_SPEED
-    //              * sum!((key,dir) in [(KeyCode::W, state.front),
-    //                              (KeyCode::A, -state.right),
-    //                              (KeyCode::S, -state.front),
-    //                              (KeyCode::D, state.right),
-    //                              (KeyCode::LeftShift, state.up),
-    //                              (KeyCode::LeftControl, -state.up)]
-    //               , is_key_down(key) ,dir);
+    let front = vec3_from_spherical_coords(state.yaw, state.pitch);
+    let right = front.cross(WORLD_UP).normalize();
+    let up = right.cross(front).normalize();
     let change = MOVE_SPEED
-                 * sum(comp!(dir, (key, dir) in [(KeyCode::W, state.front),
-                                                 (KeyCode::A, -state.right),
-                                                 (KeyCode::S, -state.front),
-                                                 (KeyCode::D, state.right),
-                                                 (KeyCode::LeftShift, state.up),
-                                                 (KeyCode::LeftControl, -state.up)]
+                 * sum(comp!(dir, (key, dir) in [(KeyCode::W, front),
+                                                 (KeyCode::A, -right),
+                                                 (KeyCode::S, -front),
+                                                 (KeyCode::D, right),
+                                                 (KeyCode::LeftShift, up),
+                                                 (KeyCode::LeftControl, -up)]
                              , is_key_down(key)));
     let mouse_position = Vec2::from(mouse_position());
     state = state.update(change, mouse_position, delta);
@@ -194,19 +169,11 @@ async fn main() {
     // Going 3d!
 
     set_camera(&Camera3D { position: state.position,
-                           up: state.up,
-                           target: state.position + state.front,
+                           up,
+                           target: state.position + front,
                            ..Default::default() });
 
     draw_grid(20, 1., DARKGRAY, GRAY);
-
-    // draw_line_3d(vec3(state.x, 0.0, state.x),
-    //              vec3(5.0, 5.0, 5.0),
-    //              Color::new(1.0, 1.0, 0.0, 1.0));
-
-    // draw_cube_wires(vec3(0., 1., -6.), vec3(2., 2., 2.), GREEN);
-    // draw_cube_wires(vec3(0., 1., 6.), vec3(2., 2., 2.), BLUE);
-    // draw_cube_wires(vec3(2., 1., 2.), vec3(2., 2., 2.), RED);
 
     for Planet { pos, color, radius, .. } in &state.planets.0 {
       draw_sphere(*pos, *radius, None, *color);
@@ -215,7 +182,6 @@ async fn main() {
     // Back to screen space, render some text
 
     set_default_camera();
-    // draw_text(a().to_string().as_str(), 10.0, 20.0, 30.0, BLACK);
     draw_text(format!("Press <TAB> to toggle mouse grab: {}", state.grabbed).as_str(),
               10.0,
               48.0 + 42.0,
